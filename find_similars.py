e7f6e543e0ea1adfddbedc6eaf7fa11f1aeb91b5
ivonr7 <isaacvr123@icloud.com>
2024-07-13 16:59:56 -0700

    added comments

--- find_similars.py
+++ find_similars.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 import numpy as np
 from time import sleep
 
+#meant to visualize similarity space learn networkx still 
 class SimilarityGraph:
     def __init__(self,*,hit_thresh:float=0.8,encoder:str = "all-MiniLM-L6-v2") -> None:
         self.graph = nx.Graph()
@@ -30,16 +31,20 @@ class SimilarityGraph:
                     sim = cos_sim(e,n['embedding'])
                     if sim > sim_graph.thresh:
                         sim_graph.add_edge()
-                     
+
+
+# Naive implementation bad n*2 algorith
+#checks up to max_iter questions against dataset to find similar ones
+#returns a dictionary of each question with the indexes of the simialr questions
 def brute_force_similars(df:pd.DataFrame,*,MAX_ITER = 10, thresh = 0.8) -> dict:
     similars = dict()
 
-    for i in tqdm(range(min(df.shape[0],MAX_ITER)),desc="Finding Similars"):
+    for i in range(min(df.shape[0],MAX_ITER)):
         similars[df.iloc[i]['question']] = []
         e1 = df.iloc[i]['embedding']
         similarities = np.array(cos_sim(e1, df['embedding']))
-        for j in range(similarities.shape[1]):
-            if  similarities[0,j] > thresh and  similarities[0,j] < 1 and i != j:
+        for j in tqdm(range(similarities.shape[1]),desc=f'Finding similars for question {i}'):
+            if  abs(similarities[0,j]) > thresh  and i != j:
                 similars[df.iloc[i]['question']].append(j)
                 
     return similars
@@ -47,15 +52,16 @@ def brute_force_similars(df:pd.DataFrame,*,MAX_ITER = 10, thresh = 0.8) -> dict:
 
 if __name__ == "__main__":
     df = pd.read_json('embedded_questions.json',orient='columns')
-    max_iter = 10
+    max_iter = 100
     ishit = 0.8
     similar_questions = brute_force_similars(df,MAX_ITER=max_iter,thresh=ishit)
     for key in similar_questions.keys():
+        print(f"Question: {key}")
         print(f"Similar indexes: {similar_questions[key]}")
 
         for similar in similar_questions[key]:
             print(similar)
-            print(df['question'][similar])
+            print(df.iloc[similar]['question'])
         
 
         input("next.. [hit enter]")
