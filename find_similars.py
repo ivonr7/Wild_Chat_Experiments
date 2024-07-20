d0931143f15934ce4dd867ec7ace69d593b150e9
ivonr7 <isaacvr123@icloud.com>
2024-07-20 12:18:29 -0700

    added graph view for cache hits

--- find_similars.py
+++ find_similars.py
@@ -1,12 +1,10 @@
 import networkx as nx
 from typing import Iterable
 from sentence_transformers import SentenceTransformer
-from sentence_transformers.util import cos_sim
+from utils import cos_sim
 import pandas as pd
 from tqdm import tqdm
 import numpy as np
-from time import sleep
-
 #meant to visualize similarity space learn networkx still 
 class SimilarityGraph:
     def __init__(self,*,hit_thresh:float=0.8,encoder:str = "all-MiniLM-L6-v2") -> None:
@@ -16,21 +14,25 @@ class SimilarityGraph:
 
 
     @staticmethod
-    def ingest(strings:Iterable,*,n_questions:int=-1):
-        sim_graph = SimilarityGraph()
-        to_embed = strings[:n_questions] if n_questions>0 else strings
+    def ingest(strings:Iterable,*,n_questions:int=-1,thresh:int=0.8):
+        sim_graph = SimilarityGraph(hit_thresh=thresh)
+        to_embed = [strings[i] for i in  range(n_questions)] if n_questions>0 else strings
         embeddings = sim_graph.model.encode(
             to_embed,
             show_progress_bar=True,
             convert_to_numpy=True
         )
-        for i,(q, e) in tqdm(enumerate(zip(to_embed,embeddings))):
-            sim_graph.add_node(i,question = q,embedding = e)
+        for i,(q, e) in tqdm(enumerate(zip(to_embed,embeddings)),desc='Attaching Nodes'):
+            sim_graph.graph.add_node(i,title = q,embedding = e.tolist(),font = {'size': 8})
             for n in sim_graph.graph:
-                if sim_graph.nodes[i] != n:
-                    sim = cos_sim(e,n['embedding'])
+                if i != n:
+                    sim = cos_sim(e,sim_graph.graph.nodes[n]['embedding'])
+                    #add edge fo similar nodes. weight is the distance between them
                     if sim > sim_graph.thresh:
-                        sim_graph.add_edge()
+                        sim_graph.graph.add_edge(i,n,title = 1-sim) 
+
+        return sim_graph
+        
 
 
 # Naive implementation bad n*2 algorith
@@ -42,7 +44,7 @@ def brute_force_similars(df:pd.DataFrame,*,MAX_ITER = 10, thresh = 0.8) -> dict:
     for i in range(min(df.shape[0],MAX_ITER)):
         similars[df.iloc[i]['question']] = []
         e1 = df.iloc[i]['embedding']
-        similarities = np.array(cos_sim(e1, df['embedding']))
+        similarities = np.abs(np.array(cos_sim(e1, df['embedding'])))
         for j in tqdm(range(similarities.shape[1]),desc=f'Finding similars for question {i}'):
             if  abs(similarities[0,j]) > thresh  and i != j:
                 similars[df.iloc[i]['question']].append(j)
@@ -51,20 +53,29 @@ def brute_force_similars(df:pd.DataFrame,*,MAX_ITER = 10, thresh = 0.8) -> dict:
 
 
 if __name__ == "__main__":
-    df = pd.read_json('embedded_questions.json',orient='columns')
-    max_iter = 100
+    from pyvis.network import Network
+    df = pd.read_json('wildchat_data_29_days.json',orient='columns')
+    max_iter = 5000
     ishit = 0.8
-    similar_questions = brute_force_similars(df,MAX_ITER=max_iter,thresh=ishit)
-    for key in similar_questions.keys():
-        print(f"Question: {key}")
-        print(f"Similar indexes: {similar_questions[key]}")
 
-        for similar in similar_questions[key]:
-            print(similar)
-            print(df.iloc[similar]['question'])
+    simGraph = SimilarityGraph.ingest(df['question'],n_questions=max_iter,thresh=ishit)
+    
+    nt = Network(width='100%',height='600px')
+    nt.from_nx(simGraph.graph)
+    nt.show_buttons(True)
+    nt.show('simgraph.html',notebook=False)
+
+    # similar_questions = brute_force_similars(df,MAX_ITER=max_iter,thresh=ishit)
+    # for key in similar_questions.keys():
+    #     print(f"Question: {key}")
+    #     print(f"Similar indexes: {similar_questions[key]}")
+
+    #     for similar in similar_questions[key]:
+    #         print(similar)
+    #         print(df.iloc[similar]['question'])
         
 
-        input("next.. [hit enter]")
+    #     input("next.. [hit enter]")
 
                 
         
